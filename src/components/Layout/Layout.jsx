9f30cb29e2bc3f4343d88eb86966b11cc1ec39c3
Ignacio Andres Mongi <ignaciomg3@gmail.com>
2026-02-28 22:02:24 -0300

    saco icono de perfil

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -18,7 +18,6 @@ const Navbar = () => {
                     {/* Logo */}
                     <div className="flex-shrink-0 flex items-center gap-2 w-full sm:w-auto justify-center sm:justify-start">
                         <div className="w-10 h-10 rounded-xl bg-blue-50 border border-blue-100 shadow-sm flex items-center justify-center flex-shrink-0 text-xl overflow-hidden">
-                            <span>📊</span>
                         </div>
                         <span className="font-bold text-2xl text-slate-800 tracking-tight">Grafiquitos</span>
                     </div>
